bdba9b52fe69395b87f374a0da5f4232152ebb21
youxunlin <youxun.lin@360gst.com>
2017-03-15 18:33:31 +0800

    proxy设置

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var cgiBin = require('./routes/cgiBin');
 
 var app = express();
 
+//设置代理
+var proxy = require('http-proxy-middleware');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,8 +27,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api', users);
-app.use('/api', cgiBin);
+// app.use('/api', users);
+// app.use('/api', cgiBin);
+// app.use('/api', proxy({
+//   target: 'http://127.0.0.1:8788',
+//   changeOrigin: true,
+//   headers: {                      //添加token,用于开发
+//    'x-api-version': 1
+//   }
+// }));
+app.use('/search', proxy({
+  target: 'https://m.gstzy.cn',
+  changeOrigin: true,
+  headers: {                      //添加token,用于开发
+   'x-api-version': 1
+ },
+ pathRewrite: {'^/search' : '/search'}
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
